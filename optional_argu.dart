17f3bb6f7646000ea4bd9fbef654a4eb766c8c50
mekritbhagat <34103079+mekritbhagat@users.noreply.github.com>
2020-03-14 13:24:28 +0545

    Create optional_argu.dart

---
+++ optional_argu.dart
@@ -0,0 +1,8 @@
+void main() {
+  void printCountries(String name1, String name2, [String name3]) {
+    print(name1);
+    print(name2);
+    print(name3);
+  }
+  printCountries("India", "Japan");
+}
