111532ce523e41920637d936fa0d7f6040f51383
mekritbhagat <34103079+mekritbhagat@users.noreply.github.com>
2020-03-14 13:12:03 +0545

    Create if_else.dart

---
+++ if_else.dart
@@ -0,0 +1,8 @@
+void main() {
+  var salary = 25000;
+  if (salary >= 20000 && salary < 100000) {
+    print("Congratulations!! got promotion");
+  } else {
+  print("You need to work hard");
+  }
+}
