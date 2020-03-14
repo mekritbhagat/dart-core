3863ac05fec6e8d65e42e3c478eaf6843a082754
mekritbhagat <34103079+mekritbhagat@users.noreply.github.com>
2020-03-14 13:15:38 +0545

    Create switch_case.dart

---
+++ switch_case.dart
@@ -0,0 +1,14 @@
+void main() {
+  String grade = 'A';
+  switch(grade) {
+    case 'A':
+      print("Excellent");
+      break;
+    case 'B':
+      print("Very Good");
+      break;
+    default:
+      print("Good");
+      break;
+  }
+}
