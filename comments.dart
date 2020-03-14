e43e659c91f4a50a27720d208d876d33f370e764
mekritbhagat <34103079+mekritbhagat@users.noreply.github.com>
2020-03-14 13:09:26 +0545

    Create comments.dart

---
+++ comments.dart
@@ -0,0 +1,14 @@
+void main() {
+  print("Hello World");
+  print("This is new message");
+  print(false);
+  print(12 / 3);
+  print("This is comment line // or /* */");
+  int calculate() {
+    return 6 * 7;
+  }
+  print(calculate());
+  //Dart data-types: Number(int, double), String,Boolean, List, Map, Rune, Symbol
+  /* All data types in Dart are Objects
+  Therefore, values are by default null */
+}
